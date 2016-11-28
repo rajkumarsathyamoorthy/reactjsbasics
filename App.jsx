dafbd840747ba1781f3fef2bcb97a722c5df0a5d
rajkumar <vsrajkumar@live.com>
2016-11-28 12:21:30 +0530

    28/11/2016

--- App.jsx
+++ App.jsx
@@ -70,6 +70,25 @@ handleClick: function(e)
      inner: ["ranipet","arcot","walaja"]
    }]
 
+   var libraries = [
+
+       { name: 'Backbone.js', url: 'http://documentcloud.github.io/backbone/'},
+       { name: 'AngularJS', url: 'https://angularjs.org/'},
+       { name: 'jQuery', url: 'http://jquery.com/'},
+       { name: 'Prototype', url: 'http://www.prototypejs.org/'},
+       { name: 'React', url: 'http://facebook.github.io/react/'},
+       { name: 'Ember', url: 'http://emberjs.com/'},
+       { name: 'Knockout.js', url: 'http://knockoutjs.com/'},
+       { name: 'Dojo', url: 'http://dojotoolkit.org/'},
+       { name: 'Mootools', url: 'http://mootools.net/'},
+       { name: 'Underscore', url: 'http://documentcloud.github.io/underscore/'},
+       { name: 'Lodash', url: 'http://lodash.com/'},
+       { name: 'Moment', url: 'http://momentjs.com/'},
+       { name: 'Express', url: 'http://expressjs.com/'},
+       { name: 'Koa', url: 'http://koajs.com/'},
+
+   ]
+
    var mydata = [
    {
       name: "Rajkumar",
@@ -112,7 +131,7 @@ handleClick: function(e)
             <Footer biodata={biodata} greet={this.onAlert} age={23} footertext={"Copyrighted by Rajkumar 2013"} />
             <Footers biodatas={biodatas} footertext={"Copyrighted by Raji 2013"} />
             <Changeit myname="raj"/>
-            <Myfilterapp listy={arealist} />
+            <Myfilterapp items={libraries}/>
             <DisplayContainer3/>
 
          </div>
--- temp/filtersearch.jsx
+++ temp/filtersearch.jsx
@@ -1,15 +1,56 @@
 import React from 'react';
 
-export default class Myfilterapp extends React.Component {
-    render() {
-        return (
-            <div className="badge">
-                <h1>Filter Search in React</h1>
-                <input type="text"/>
-                <ul>
-                  {this.props.listy.map((areas) => <li key={areas.areas}>{areas.areas}</li>)}
-                </ul>
-            </div>
-        )
+// Let's create a "real-time search" component
+
+var Myfilterapp = React.createClass({
+
+
+    getInitialState: function() {
+        return {
+          searchString: ''
+        };
+    },
+
+    handleChange: function(e) {
+
+        // If you comment out this line, the text box will not change its value.
+        // This is because in React, an input cannot change independently of the value
+        // that was assigned to it. In our case this is this.state.searchString.
+
+        this.setState({
+          searchString: e.target.value
+        });
+    },
+
+    render: function() {
+
+        var libraries = this.props.items,
+            searchString = this.state.searchString.trim().toLowerCase();
+
+        if (searchString.length > 0) {
+
+            // We are searching. Filter the results.
+
+            libraries = libraries.filter(function(l) {
+                return l.name.toLowerCase().match(searchString);
+            });
+
+        }
+
+        return <div>
+            <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Type here"/>
+
+            <ul>
+                {libraries.map((l) => {
+                    return <li key={l.name}>{l.name}
+                        <a href={l.url}>{l.url}</a>
+                    </li>
+                })}
+            </ul>
+
+        </div>;
+
     }
-}
+});
+
+export default Myfilterapp;
